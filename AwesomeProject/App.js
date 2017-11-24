b9f4fe08882c6a1567e0d159d81c278d46a599ba
mdaisuke <daisuke.mori@me.com>
2017-11-25 00:06:29 +0900

    react native sample

---
+++ AwesomeProject/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View, AppRegistry, Image } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+
+    return (
+      <View style={styles.container}>
+        <Text>Hello worold!!!!</Text>
+        <Image source={pic} style={{width: 193, height: 110}} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
