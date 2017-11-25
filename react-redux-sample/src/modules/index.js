f8d25ce4624eef3119dd42f91c85fe3e6aeab2ac
mdaisuke <daisuke.mori@me.com>
2017-11-25 10:13:34 +0900

    initialize and containers

--- react-redux-sample/src/modules/index.js
+++ react-redux-sample/src/modules/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from 'react-router-redux'
 
-export default combineReducer({
+export default combineReducers({
   routing: routerReducer
 })
